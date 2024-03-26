e97dadedadb0063968d65e52d3283859b2916527
Allan <allankiplagatkipkemei@gmail.com>
2024-03-26 23:59:00 +0300

    *

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,11 @@ app.use(bodyParser.json({ limit: '10mb', extended: true }))
  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors({
+
+    origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}))
 
 mongoose
     .connect(process.env.MONGO_URL, {
