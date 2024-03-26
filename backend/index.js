6722aa8ed489c7ea04cbb06bbe1cb9b833ae0e98
Allan <allankiplagatkipkemei@gmail.com>
2024-03-27 00:48:11 +0300

    *

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({ limit: '10mb', extended: true }))
 app.use(express.json({ limit: '10mb' }))
 app.use(cors({
 
-    origin: 'http://localhost:3000',
+    origin: 'https://electronic-school-5k2n68sgc-allan-kipkemei.vercel.app/',
   optionsSuccessStatus: 200
 }))
 
